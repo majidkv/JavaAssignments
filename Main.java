626053c979ebd2eca754c0fb150cd1c11814fd05
Madjid Koochaki <madjid.koochaki@gmail.com>
2020-05-24 00:58:27 -0400

    java course assignment initial

---
+++ Main.java
@@ -0,0 +1,33 @@
+
+/* Java class*******Assignment number 1
+        System.out.println("This is text");
+        System.out.println('A');
+        System.out.println(53);
+        System.out.println(23.08f);
+        System.out.println(1.97);
+        System.out.println(true);
+ */
+
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String myString ="This is text";
+        char myChar = 'A';
+        short myShortVar = 53;
+        float myFloat1 = 23.08f;
+        double myFloat2 = 1.97;
+        boolean myBoolean = true;
+
+
+        System.out.println(myString);
+        System.out.println(myChar);
+        System.out.println(myShortVar);
+        System.out.println(myFloat1);
+        System.out.println(myFloat2);
+        System.out.println(myBoolean);
+    }
+}
